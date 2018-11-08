7a7484d176462c649c664ed97aeed7a1fee21ef7
Chris Carlson <christopher.s.carlson@wmich.edu>
2018-11-08 11:24:50 -0500

    Just removed some uneccesary code.

--- main.h
+++ main.h
@@ -3,9 +3,6 @@
 #include <stdbool.h> // For the definitions of false and true
 #include <unistd.h> // For the sleep function
 
-// volatile sig_atomic_t alarmSig;
-// volatile sig_atomic_t intSig;
 volatile int alarmsHandled;
-volatile int intsHandled;
 void alarmHandler(int sigVal);
 void intHandler(int sigVal);

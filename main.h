05598f92212d2f35fb87009ecb6e29a4d56853bb
Chris Carlson <43390136+ChristopherCS@users.noreply.github.com>
2018-11-08 11:56:30 -0500

    Added comments

--- main.h
+++ main.h
@@ -1,7 +1,11 @@
-#include <signal.h> // For the 'signal' function
-#include <stdio.h>	// For printf
+// main.h: Header file for a signal handler demonstration in c. 
+//     By: Chris Carlson
+//   Date: November 8, 2018
+
+#include <signal.h>  // For the 'signal' function
+#include <stdio.h>	 // For printf
 #include <stdbool.h> // For the definitions of false and true
-#include <unistd.h> // For the sleep function
+#include <unistd.h>  // For the sleep function
 
 volatile int alarmsHandled;
 void alarmHandler(int sigVal);

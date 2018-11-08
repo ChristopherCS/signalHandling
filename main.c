1d16bc361767d692786e33741a6d6009d876c2d4
Chris Carlson <43390136+ChristopherCS@users.noreply.github.com>
2018-11-08 11:52:00 -0500

    Fixed format of comments.

--- main.c
+++ main.c
@@ -2,10 +2,10 @@
 //          By:  Chris Carlson
 //        Date:  November 8, 2018
 // Description:  The program handles two alarm signals (first after 3 seconds, second after 2 seconds). 
-// 							 When it handles the alarm signal it prints,"in alarm". After two alarms pauses and waits   
-//							 for a SIGINT to come from the user. When the user gives the interrupt signal, the handler
-// 							 writes, "control c handled". The program detects that control c was handled and then
-// 							 it pauses again. The program is killed by a kill signal. 
+//               When it handles the alarm signal it prints,"in alarm". After two alarms pauses and waits   
+//               for a SIGINT to come from the user. When the user gives the interrupt signal, the handler
+//               writes, "control c handled". The program detects that control c was handled and then
+//               it pauses again. The program is killed by a kill signal. 
 
 #include "main.h"
 
